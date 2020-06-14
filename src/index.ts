847be4b576cc9ab66692f6dccae2ca7328dff218
Fabian Cook <hello@fabiancook.dev>
2020-06-14 17:15:09 +1200

    transact

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import {
     DefaultDataFactory,
     isDefaultGraph, isQuadGraph,
     Quad, QuadGraph,
-    QuadGraphLike,
+    QuadGraphLike, QuadObject,
     QuadSubject
 } from "@opennetwork/rdf-data-model"
 import * as ns from "./namespace"
@@ -10,37 +10,62 @@ import { literal } from "@opennetwork/rdf-namespace-javascript"
 
 export interface TransactionOptions {
     context?: QuadGraphLike
-    skip?: (quad: Quad, context: QuadGraph) => boolean | Promise<boolean>
-    header?: (id: QuadSubject, quad: Quad, context: QuadGraph) => AsyncIterable<Quad> | Iterable<Quad>
-    footer?: (id: QuadSubject, quad: Quad, context: QuadGraph) => AsyncIterable<Quad> | Iterable<Quad>
+    quadHeader?: (id: QuadSubject, context: QuadGraph, quad: Quad) => AsyncIterable<Quad> | Iterable<Quad>
+    quadFooter?: (id: QuadSubject, context: QuadGraph, quad: Quad) => AsyncIterable<Quad> | Iterable<Quad>
+    header?: (id: QuadSubject, context: QuadGraph) => AsyncIterable<Quad> | Iterable<Quad>
+    footer?: (id: QuadSubject, context: QuadGraph) => AsyncIterable<Quad> | Iterable<Quad>
+    defaultGraph?: QuadObject
 }
 
-export function transaction(options: TransactionOptions): ((input: AsyncIterable<Quad>) => AsyncIterable<Quad>) {
+function getContext(options: TransactionOptions): QuadGraph {
     const context = options.context ? DefaultDataFactory.fromTerm(options.context) : ns.transaction
     if (!isQuadGraph(context)) {
         throw new Error("Invalid context")
     }
-    return async function *transaction(input: AsyncIterable<Quad>): AsyncIterable<Quad> {
-        for await (const quad of input) {
-            if (options.skip) {
-                if (await options.skip(quad, context)) {
-                    continue
-                }
-            }
-            const id = DefaultDataFactory.blankNode()
-            yield new Quad(id, ns.type, ns.transaction, context)
-            yield new Quad(id, ns.subject, quad.subject, context)
-            yield new Quad(id, ns.predicate, quad.predicate, context)
-            yield new Quad(id, ns.object, quad.object, context)
-            yield new Quad(id, ns.graph, isDefaultGraph(quad.graph) ? ns.defaultGraph : quad.graph, context)
-            yield new Quad(id, ns.timestamp, literal(new Date()), context)
-            if (options.header) {
-                yield* options.header(id, quad, context)
-            }
-            yield quad
-            if (options.footer) {
-                yield* options.footer(id, quad, context)
-            }
+    return context
+}
+
+export async function *transact(options: TransactionOptions, source: (id: QuadSubject) => AsyncIterable<Quad>): AsyncIterable<Quad> {
+    const context = getContext(options)
+    const id = DefaultDataFactory.blankNode()
+    yield new Quad(id, ns.type, ns.transaction, context)
+    yield new Quad(id, ns.timestamp, literal(new Date()), context)
+    if (options.header) {
+        yield* options.header(id, context)
+    }
+    for await (const quad of source(id)) {
+        const quadId = DefaultDataFactory.blankNode()
+        yield new Quad(id, ns.quad, quadId, context)
+        yield new Quad(quadId, ns.type, ns.quad, context)
+        yield new Quad(quadId, ns.quadSubject, quad.subject, context)
+        yield new Quad(quadId, ns.quadPredicate, quad.predicate, context)
+        yield new Quad(quadId, ns.quadObject, quad.object, context)
+        yield new Quad(quadId, ns.quadGraph, isDefaultGraph(quad.graph) ? (options.defaultGraph || ns.quadDefaultGraph) : quad.graph, context)
+        yield new Quad(quadId, ns.timestamp, literal(new Date()), context)
+        if (options.quadHeader) {
+            yield* options.quadHeader(quadId, context, quad)
         }
+        yield quad
+        if (options.quadFooter) {
+            yield* options.quadFooter(quadId, context, quad)
+        }
+    }
+    if (options.footer) {
+        yield* options.footer(id, context)
+    }
+}
+
+export function transaction(options: TransactionOptions): ((input: AsyncIterable<Quad>) => AsyncIterable<Quad>) {
+    const context = getContext(options)
+    return async function *transaction(input: AsyncIterable<Quad>): AsyncIterable<Quad> {
+        yield* transact(
+            {
+                ...options,
+                context
+            },
+            async function *source() {
+                yield* input
+            }
+        )
     }
 }
--- src/namespace.ts
+++ src/namespace.ts
@@ -4,9 +4,11 @@ import {DefaultDataFactory} from "@opennetwork/rdf-data-model";
 
 export const type = DefaultDataFactory.namedNode("http://www.w3.org/1999/02/22-rdf-syntax-ns#type")
 export const transaction = DefaultDataFactory.namedNode("https://types.open-network.dev/rdf-transaction/transaction")
-export const subject = DefaultDataFactory.namedNode("https://types.open-network.dev/rdf-transaction/transaction/subject")
-export const predicate = DefaultDataFactory.namedNode("https://types.open-network.dev/rdf-transaction/transaction/predicate")
-export const object = DefaultDataFactory.namedNode("https://types.open-network.dev/rdf-transaction/transaction/object")
-export const graph = DefaultDataFactory.namedNode("https://types.open-network.dev/rdf-transaction/transaction/graph")
-export const defaultGraph = DefaultDataFactory.namedNode("https://types.open-network.dev/rdf-transaction/transaction/default-graph")
 export const timestamp = DefaultDataFactory.namedNode("https://types.open-network.dev/rdf-transaction/transaction/timestamp")
+
+export const quad = DefaultDataFactory.namedNode("https://types.open-network.dev/rdf-transaction/quad")
+export const quadSubject = DefaultDataFactory.namedNode("https://types.open-network.dev/rdf-transaction/quad/subject")
+export const quadPredicate = DefaultDataFactory.namedNode("https://types.open-network.dev/rdf-transaction/quad/predicate")
+export const quadObject = DefaultDataFactory.namedNode("https://types.open-network.dev/rdf-transaction/quad/object")
+export const quadGraph = DefaultDataFactory.namedNode("https://types.open-network.dev/rdf-transaction/quad/graph")
+export const quadDefaultGraph = DefaultDataFactory.namedNode("https://types.open-network.dev/rdf-transaction/quad/default-graph")
